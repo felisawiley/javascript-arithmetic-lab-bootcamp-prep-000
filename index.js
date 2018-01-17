e7f8d5922a21ec9d43cfd1f144981e368f103b0a
Felisa Wiley <felisawiley@gmail.com>
2018-01-17 19:44:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ var number = 10
 function add5() {
   number += 5
 }
+function divideby3() {
+  number /+ 3 
+
+}
+
+divideby3()
+
+console.log(number)
+
+add5()
+
+console.log(number)
+
+number = 10
+add5()
+console.log(number)
+
+divide
